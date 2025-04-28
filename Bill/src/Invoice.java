081ff0f15ad8c5c82067321b35d1810349577e6e
Devon-888 <devoneyc-wm24@student.tarc.edu.my>
2025-04-28 15:44:33 +0800

    a

---
+++ Bill/src/Invoice.java
@@ -0,0 +1,92 @@
+import java.util.Date;
+
+public class Invoice{
+    private String invoiceID;
+    private String paymentID;
+    private String customerName;
+    private String seatNumber;
+    private String movieTitle;
+    private double totalPrice;
+    private double finalAmount;
+    private Date date;
+
+    public Invoice(String invoiceID, String paymentID, String customerName, String seatNumber,
+                   String movieTitle, double totalPrice, double finalAmount, Date date) {
+        this.invoiceID = invoiceID;
+        this.paymentID = paymentID;
+        this.customerName = customerName;
+        this.seatNumber = seatNumber;
+        this.movieTitle = movieTitle;
+        this.totalPrice = totalPrice;
+        this.finalAmount = finalAmount;
+        this.date = date;
+    }
+
+    public String getInvoiceID(){
+        return invoiceID;
+    }
+
+    public void seyInvoiceID(String invoiceID){
+        this.invoiceID = invoiceID;
+    }
+
+    public String getPaymentID(){
+        return paymentID;
+    }
+    
+    public void setPaymentID(){
+        this.paymentID = paymentID;
+    }
+
+    public String getCustomerName(){
+        return customerName;
+    }
+
+    public void setCustomerName(){
+        this.customerName = customerName;
+    }
+
+    public String getSeatNumber(){
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber){
+        this.seatNumber = seatNumber;
+    }
+
+    public String getMovieTitle(){
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle){
+        this.movieTitle = movieTitle;
+    }
+
+    public double getTotalPrice(){
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice){
+        this.totalPrice = totalPrice;
+    }
+
+    public double getFinalAmount(){
+        return finalAmount;
+    }
+
+    public void setFinalAmount(double finalAmount){
+        this.finalAmount = finalAmount;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+
+    public double calculateFinalAmount() {
+        return finalAmount;
+    }
+}
---
+++ Bill/src/Ticket.java
@@ -0,0 +1,47 @@
+public class Ticket {
+    private int ticketID;
+    private Customer customer;
+    private Payment payment;
+
+    public Ticket(int ticketID, Customer customer, Payment payment) {
+        this.ticketID = ticketID;
+        this.customer = customer;
+        this.payment = payment;
+    }
+
+    public int getTicketID(){
+        return ticketID;
+    }
+
+    public void setTicketID(int ticketID){
+        this.ticketID = ticketID;
+    }
+
+    public Customer getCustomer(){
+        return customer;
+    }
+
+    public void setCustomer(Customer customer){
+        this.customer = customer;
+    }
+
+    public Payment getPayment(){
+        return payment;
+    }
+
+    PUBLIC void setPayment(Payment payment){
+        this.payment = payment;
+    }
+
+    public void generateTicket() {
+        System.out.println("Ticket Owner: " + customer.getName());
+    }
+
+    public void cancelTicket() {
+        System.out.println("Ticket ID " + ticketID + " has been cancelled.");
+    }
+
+    public void checkTicketStatus() {
+        System.out.println("Checking ticket status for Ticket ID: " + ticketID);
+    }
+}
